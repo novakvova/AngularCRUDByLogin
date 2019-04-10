af9835d05492660a5ba9d97ae5bc36fc9a777588
novakvova <novakvova@gmail.com>
2019-04-10 15:18:51 +0300

    Add Validation backend

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -11,15 +11,25 @@ import {ApiService} from '../core/api.service';
 export class AddUserComponent implements OnInit {
 
   addForm: FormGroup;
+  submitted = false;
+  loading = false;
+  error = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    age: '',
+    salary: ''
+  };
   constructor(private formBuilder: FormBuilder,
               private router: Router,
               private apiService: ApiService) { }
 
   ngOnInit() {
+    const regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$';
     this.addForm = this.formBuilder.group({
-      id: [],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       age: ['', Validators.required],
@@ -27,11 +37,26 @@ export class AddUserComponent implements OnInit {
     });
   }
 
-  onSubmit() {
-    this.apiService.createUser(this.addForm.value)
-      .subscribe( data => {
-        this.router.navigate(['list-user']);
-      });
-  }
+  // convenience getter for easy access to form fields
+  get f() { return this.addForm.controls; }
 
+  onSubmit() {
+
+    this.submitted = true;
+    if (this.addForm.invalid) {
+      return;
+    }
+    this.loading = true;
+
+    this.apiService.createUser(this.addForm.value)
+      .subscribe(
+      data => {
+        this.router.navigate(['list-user']);
+      },
+      badReasponse => {
+        this.error = badReasponse.error;
+        console.log('----error-----', this.error);
+      }
+    );
+  }
 }
